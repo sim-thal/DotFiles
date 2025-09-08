fac6dba8d89d3d80de95da730163543b3323e6ef
sim-thal <pripyat42@gmail.com>
2025-09-08 17:12:12 +0200

    ...

--- oneliner.sh
+++ oneliner.sh
@@ -1,4 +1,4 @@
 curl -o install.sh https://raw.githubusercontent.com/sim-thal/DotFiles/refs/heads/main/install.sh;
-chmod +x install.sh;
+sudo chmod +x install.sh;
 pacman -Sy kitty;
 kitty install.sh;
